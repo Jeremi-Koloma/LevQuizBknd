eded9343e2c11ff7e01c98f7c0baf8ac05eb4e6a
jkkoloma <kolomajeremi60@gmail.com>
2023-01-04 18:20:36 +0000

    Ajout des AccountService et QuizService

---
+++ src/main/java/com/LevQuiz/LevQuiz/Services/AccountService.java
@@ -0,0 +1,45 @@
+package com.LevQuiz.LevQuiz.Services;
+
+import com.LevQuiz.LevQuiz.Models.AppUser;
+import com.LevQuiz.LevQuiz.Models.Role;
+
+import java.util.List;
+
+public interface AccountService {
+    // Une methode qui va prendre l'utilisateur en parmètre pour l'Enregistrer
+    void saveUser(AppUser appUser);
+
+    // Une méthode qui va retourner un utilisateur par son Nom pour authentification
+    AppUser findByUsername(String username);
+
+    // Une méthode qui va retourner un utilisateur par son Email
+    AppUser findByEmail(String email);
+
+    // Une méthode qui va retourné une liste d'utilisateurs
+    List<AppUser> userList();
+
+    // Une méthode qui va retourné un Role d'utilisateur
+    Role findUserRoleByName(String role);
+
+    // Une méthode qui va retourné un Role avec actuel Role en paramètre pour l'Enregistrer
+    Role saveRole(Role role);
+
+    // Une méthode pour mèttre l'utilisateur à jours
+    void updateUser(AppUser appUser);
+
+    // Une méthode qui va retourné un utilisateur par son ID
+    AppUser findById(Long id);
+
+    // Une méthode pour supprimer un utilisateur avec actuel utilisateur en paramètre
+    void deleteUser(AppUser appUser);
+
+    // Une méthode pour Changer le mots de passe d'un utilisateur avec actuel utilisateur en paramètre
+    void resetPassword(AppUser appUser);
+
+    // Une méthode qui va retourné la liste des utilisateur par leurs Non d'utilisateur pour type barre de Recherche
+    List<AppUser> getUserListByUsername(String username);
+
+    // un simple savegarde des utilisateurs
+    void simpleSave(AppUser appUser);
+
+}
---
+++ src/main/java/com/LevQuiz/LevQuiz/Services/QuizService.java
@@ -0,0 +1,33 @@
+package com.LevQuiz.LevQuiz.Services;
+
+import com.LevQuiz.LevQuiz.Models.AppUser;
+import com.LevQuiz.LevQuiz.Models.Quiz;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+
+public interface QuizService {
+    // Une méthode pour Enregister le Quiz les paramètre :
+    // AppUser : Pour connaître l'utilisateur qui a créer le Quiz
+    // titre : Le Titre du quiz
+    Quiz saveQuiz(AppUser appUser, HashMap<String, String> request, String titre);
+
+    // Une méthode qui va retourné une liste de Quiz
+    List<Quiz> quizList();
+
+    // Une méthode qui va retouné un quiz par son ID
+    Quiz getQuizById(Long id);
+
+    // Une méthode pour retouner une liste de Quiz créer par un utilisateur
+    List<Quiz> findQuizByUsername(String username );
+
+    // Une méthode qui va retourné un Quiz pour le supprimer.
+    Quiz deleteQuiz(Quiz quiz);
+
+    // une méthode qui va retourné un string pour de Titre du Quiz
+    String saveQuizTitre(HttpServletRequest request, String quizTitre);
+
+
+
+}
