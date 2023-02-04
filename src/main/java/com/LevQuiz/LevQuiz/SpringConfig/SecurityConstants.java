682af7fdc8167267201680e9857e87191687bbb0
jkkoloma <kolomajeremi60@gmail.com>
2023-02-04 16:44:36 +0000

    rectification 04/02/

--- src/main/java/com/LevQuiz/LevQuiz/SpringConfig/SecurityConstants.java
+++ src/main/java/com/LevQuiz/LevQuiz/SpringConfig/SecurityConstants.java
@@ -3,7 +3,7 @@ package com.LevQuiz.LevQuiz.SpringConfig;
 public class SecurityConstants {
     // Déclarons des constantes que nous allons utilisé dans JwtAuthentication
     public static final String SECRET = "mySecret231090";
-    public static final long EXPIRATION_TIME = 10_1000;
+    public static final long EXPIRATION_TIME = 525948*1000; // 1ans
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_TYPE = "Authorization";
     public static final String CLIENT_DOMAIN_URL = "*";
